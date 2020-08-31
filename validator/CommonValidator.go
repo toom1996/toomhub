60c32eecd83f91fa84fd42c802aae117794ab0e3
1023150697@qq.com <1023150697@qq.com>
2020-08-31 18:00:27 +0800

    add file

--- validator/CommonValidator.go
+++ validator/CommonValidator.go
@@ -2,12 +2,16 @@
 // @Author    2020/8/31 17:35
 package validator
 
-import "github.com/go-playground/validator/v10"
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+)
 
 type CommonValidator struct {
 }
 
 func (c *CommonValidator) TransError(err validator.ValidationErrors) string {
 
+	fmt.Println(err)
 	return "参数错误"
 }
