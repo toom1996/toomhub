22d9b2ef12a0f87ce10d54759dc2afe07ebd9b88
1023150697@qq.com <1023150697@qq.com>
2020-10-18 23:16:43 +0800

    优化 - 图片预览组件

--- miniProgram/toomhubMiniProgram/pages/components/image_preview/image_preview.js
+++ miniProgram/toomhubMiniProgram/pages/components/image_preview/image_preview.js
@@ -4,13 +4,23 @@ let myStyle = `
 `
 
 Page({
-  onShareAppMessage() {
-    return {
-      title: 'swiper',
-      path: 'page/component/pages/swiper/swiper'
-    }
+  data: {
+    flag: false,
+    // 自定义顶部导航
+    navHeight: app.globalData.navHeight,
+    navTop: app.globalData.navTop,
+    show: false,
+    index: 0,
+    imageList: [],
+    image: [],
+    tmpImage: [],
+    loadedImageList:[]
   },
+
   onLoad(options) {
+    let swiperIndex = parseInt(options.index);
+    let swiperIndexLast = this.getLastIndex(swiperIndex);
+    let swiperIndexNext = this.getNextIndex(swiperIndex);
     console.log(options.index)
 
     let that = this
@@ -28,10 +38,36 @@ Page({
     });
 
     this.setData({
-      index: options.index,
-      image:options.list.split(",")
+      index: options.index
     })
-    
+
+    //插入空数组
+    let image = this.data.tmpImage;
+    let loaded = this.data.loadedImageList;
+    this.setData({
+      imageList:options.list.split(",")
+    })
+    console.log(this.data.imageList)
+    for (let i = 0; i < this.data.imageList.length; i ++) {
+      image.push('');
+      loaded.push('');
+    }
+    //修改当前索引及上下一个图片
+    image[swiperIndex] = this.data.imageList[swiperIndex];
+    loaded[swiperIndex] = 1;
+    if (swiperIndexLast >= 0) {
+      image[swiperIndex - 1] = this.data.imageList[swiperIndex - 1];
+      loaded[swiperIndex - 1] = 1;
+    }
+    if (swiperIndexNext <= this.data.imageList.length - 1) {
+      image[swiperIndex + 1] = this.data.imageList[swiperIndex + 1];
+      loaded[swiperIndex + 1] = 1;
+    }
+    this.setData({
+      image: image,
+      tmpImage: image,
+      loadedImageList: loaded
+    })
   },
 
   /**
@@ -43,40 +79,27 @@ Page({
       navTop: app.globalData.navTop,
     })
   },
-  data: {
-    flag: false,
-    // 自定义顶部导航
-    navHeight: app.globalData.navHeight,
-    navTop: app.globalData.navTop,
-
-    show: false,
-    actions: [
-      {
-        name: '发送给好友',
-        openType: 'shareToFriendHandle',
-      },
-      {
-        name: '选项',
-      },
-      {
-        name: '选项',
-        subname: '副文本',
-        openType: 'share',
-      },
-      
-    ],
-    interval: 2000,
-    duration: 200,
-    index:0,
-    image:[
-    ]
+  imageLoadedHandle(event) {
+    let loadedIndex = event.currentTarget.dataset.index;
+    let tmp = this.data.loadedImageList;
+    tmp[loadedIndex] = 1;
+    this.setData({
+      loadedImageList: tmp
+    });
+    console.log(this.data.loadedImageList)
   },
-  //长按
+  //长按事件, 抑制退出事件触发
   imageLongTapHandle(){
+    console.log(121211)
     this.setData({
       flag: true,
-      show: true,
     })
+    var _this = this
+    setTimeout(function () {
+      _this.setData({
+        flag: false,
+      })
+    }, 300) //延迟时间 这里是1秒
   },
 
   onClose() {
@@ -100,4 +123,30 @@ Page({
       })
     }
   },
+  getLastIndex(index) {
+    return index - 1;
+  },
+  getNextIndex(index) {
+    return index + 1;
+  },
+  swiperChangeHandle(event) {
+    let swiperIndex = event.detail.current
+    let swiperIndexLast = this.getLastIndex(swiperIndex);
+    let swiperIndexNext = this.getNextIndex(swiperIndex);
+    let image = this.data.tmpImage;
+    let loaded = this.data.loadedImageList;
+
+    if (swiperIndexLast >= 0) {
+      image[swiperIndex - 1] = this.data.imageList[swiperIndex - 1];
+      loaded[swiperIndex - 1] = 1;
+    }
+    if (swiperIndexNext <= this.data.imageList.length - 1) {
+      image[swiperIndex + 1] = this.data.imageList[swiperIndex + 1];
+      loaded[swiperIndex + 1] = 1;
+    }
+    this.setData({
+      image: image,
+      loadedImageList: loaded
+    })
+  }
 })
