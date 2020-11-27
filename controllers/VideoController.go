ae99cc279907eb2ac4a96cc74dbe1b1a0edc673d
1023150697@qq.com <1023150697@qq.com>
2020-11-27 18:29:14 +0800

    更新代码

--- controllers/VideoController.go
+++ controllers/VideoController.go
@@ -18,6 +18,7 @@ func (image *VideoController) Register(engine *gin.Engine) {
 	controller := engine.Group("/video")
 	{
 		controller.POST("/upload", image.Upload)
+		controller.POST("/upload1", image.Upload1)
 	}
 
 	controller.Use(middware.CheckIdentity())
@@ -63,4 +64,50 @@ func (*VideoController) Upload(context *gin.Context) {
 			"name":         res["url"],
 		},
 	})
+}
+func (*VideoController) Upload1(context *gin.Context) {
+	//rand.Seed(uint64(time.Now().UnixNano()))
+	//buf := new(bytes.Buffer)
+	//_, _ = buf.ReadFrom(context.Request.Body)
+	////fmt.Println(buf.String())
+	//
+	//randNum := strconv.Itoa(rand.Intn(1000))
+	//fileName := "test" + randNum
+	//fmt.Println(fileName)
+	//f, err1 := os.Create(fileName) //创建文件
+	//fmt.Println(err1)
+	//
+	//n, err1 := io.WriteString(f, buf.String()) //写入文件(字符串)
+	//fmt.Println(err1)
+	//fmt.Printf("写入 %d 个字节n", n)
+
+	context.JSON(200, gin.H{
+		"code":    200,
+		"message": "上传成功",
+	})
+	//config := util.GetConfig()
+	//putPolicy := storage.PutPolicy{
+	//	Scope: "toomhub",
+	//}
+	//mac := qbox.NewMac(config.Qiniu.AccessKey, config.Qiniu.SecretKey)
+	//upToken := putPolicy.UploadToken(mac)
+	//
+	//cfg := storage.Config{}
+	//// 空间对应的机房
+	//cfg.Zone = &storage.ZoneHuadong
+	//// 是否使用https域名
+	//cfg.UseHTTPS = false
+	//// 上传是否使用CDN上传加速
+	//cfg.UseCdnDomains = false
+	//
+	//resumeUploader := storage.NewResumeUploader(&cfg)
+	//ret := storage.PutRet{}
+	//putExtra := storage.RputExtra{}
+	//err := resumeUploader.PutFile(util.Ctx, &ret, upToken, key, localFile, &putExtra)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println(ret.Key, ret.Hash)
+
 }
