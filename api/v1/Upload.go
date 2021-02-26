06dfd8d5ce5909abf904863d491929bb5e5b8447
1023150697@qq.com <1023150697@qq.com>
2021-02-26 18:02:11 +0800

    更新代码

--- api/v1/Upload.go
+++ api/v1/Upload.go
@@ -4,6 +4,8 @@
 package v1
 
 import (
+	"encoding/base64"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/qiniu/api.v7/v7/auth/qbox"
 	"github.com/qiniu/api.v7/v7/storage"
@@ -13,10 +15,12 @@ import (
 
 func GetQiniuAccessToken(context *gin.Context) {
 	bucket := "toomhub"
+	n := base64.StdEncoding.EncodeToString([]byte("zawazawa-image-thumb:$(key)"))
+	fmt.Println(n)
 	putPolicy := storage.PutPolicy{
 		Scope:         bucket,
 		ReturnBody:    `{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)"}`,
-		PersistentOps: "imageView2/2/w/200/h/200|saveas/dGVzdDExMjEzOnRlc3Q=",
+		PersistentOps: "imageView2/2/w/200/h/200|saveas/" + n,
 	}
 	putPolicy.Expires = 100
 	mac := qbox.NewMac(setting.ZConfig.Qiniu.AccessKey, setting.ZConfig.Qiniu.SecretKey)
